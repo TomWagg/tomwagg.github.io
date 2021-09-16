b645c867cee8d90a2943f1d2ed36e02575904d73
Tom Wagg <twagg@college.harvard.edu>
2021-09-16 12:43:36 -0700

    add everything from the old website!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal website
+A repo to house the html/css/js that makes up my [personal website](https://www.tomwagg.com)!
---
+++ js/index.js
@@ -0,0 +1,87 @@
+// flip the profile image every time the mouse 'flicks' it
+document.querySelector("img.profile").addEventListener("mouseout", function (e) {
+    this.classList.toggle("flip");
+});
+
+// make projects bounce in, in sequence
+let i = 0;
+document.querySelectorAll(".project").forEach(function (el) {
+    // el.addEventListener("mouseenter", function() {
+    //     console.log(this);
+    //     animateCSS(this, "pulse");
+    // });
+    setTimeout(function() {
+        el.classList.add("visible")
+        animateCSS(el, "fadeIn");
+    }, i * 100);
+    i += 1;
+});
+
+// social link clickers
+document.querySelectorAll(".social-link").forEach(function(el) {
+    el.addEventListener("click", function() {
+        this.children[0].click();
+    });
+});
+
+document.querySelectorAll(".research-projects .project").forEach(function(el) {
+    el.addEventListener("click", function() {
+        const research_details = document.getElementById("research_details");
+        if (research_details.classList.contains("show")) {
+            research_details.classList.remove("show");
+
+            // if it was a different details then switch
+            if (research_details.querySelector(".details:not(.hide)") !== document.getElementById(el.getAttribute("data-details"))) {
+                setTimeout(() => {
+                    el.click()
+                }, 1000);
+            }
+        } else {
+            // remove old details if they exist
+            const old_details = document.querySelector("#research_details .details:not(.hide)")
+            if (old_details !== null) {
+                old_details.classList.add("hide");
+            }
+            
+            // switch to new details
+            document.getElementById(el.getAttribute("data-details")).classList.remove("hide");
+            research_details.classList.add("show");
+            console.log($("#projects").offset().top + $("#projects").outerHeight(true))
+            $('html, body').animate({
+                scrollTop: $("#projects").position().top + $("#projects").outerHeight(true)
+            }, 1000);
+        }
+    });
+});
+
+/**
+ * Add a CSS animation
+ * @param {*} element element to animate
+ * @param {*} animationName which animation
+ * @param {*} callback what to do on completion
+ */
+function animateCSS(element, animationName, callback) {
+    let nodes = null;
+    if (typeof (element) == "string") {
+        nodes = document.querySelectorAll(element);
+    } else {
+        nodes = [element];
+    }
+    nodes.forEach(function (node) {
+        node.classList.add('animated', animationName);
+        $(node).one("animationend", function () {
+            this.classList.remove('animated', animationName);
+            if (typeof callback === 'function') callback();
+        });
+    });
+}
+
+// let oReq = new XMLHttpRequest();
+// oReq.addEventListener("load", reqListener);
+// oReq.open("GET", "html/test.html");
+// oReq.send();
+
+// function reqListener() {
+//     console.log(this.responseText);
+//     document.getElementById("test").innerHTML = this.responseText;
+// }
