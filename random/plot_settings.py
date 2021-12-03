c8969a96affb72ed6dfdb35288417a7dcaed05d6
Tom Wagg <twagg@college.harvard.edu>
2021-12-03 13:11:39 -0800

    add convenient plot setting storage

---
+++ random/plot_settings.py
@@ -0,0 +1,18 @@
+%config InlineBackend.figure_format = 'retina'
+
+plt.rc('font', family='serif')
+plt.rcParams['text.usetex'] = False
+fs = 24
+
+# update various fontsizes to match
+params = {'figure.figsize': (12, 8),
+          'legend.fontsize': fs,
+          'axes.labelsize': fs,
+          'xtick.labelsize': 0.6 * fs,
+          'ytick.labelsize': 0.6 * fs,
+          'axes.linewidth': 1.1,
+          'xtick.major.size': 7,
+          'xtick.minor.size': 4,
+          'ytick.major.size': 7,
+          'ytick.minor.size': 4}
+plt.rcParams.update(params)
