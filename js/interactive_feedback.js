b92795b2defa9e5273af99a369f3ce7d0bb2ee18
Tom Wagg <tomjwagg@gmail.com>
2025-04-15 18:55:04 -0700

    allow switching between different models (bit janky)

--- js/interactive_feedback.js
+++ js/interactive_feedback.js
@@ -8,7 +8,9 @@ let intervalID
 let direction = 1
 let interval_time = 70
 
-make_plot()
+const models = ['fiducial', 'ce-0.1', 'ce-10.0', 'qcritB-0.0', 'qcritB-1000.0', 'beta-0.0']
+
+build_sn_dists_histogram('fiducial')
 
 this.window.addEventListener('load', function () {
     document.getElementById('dark-mode-checkbox').addEventListener('change', function () {
@@ -24,6 +26,98 @@ this.window.addEventListener('load', function () {
                 'yaxis.gridcolor': this.checked ? '#595656' : '#eee',
             })
         })
+    })
+
+    const range = document.getElementById('sn-dists-slider')
+    const bubble = document.getElementById('sn-dists-slider-bubble')
+    const max_times = [
+        4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28, 28.5, 29, 29.5, 30, 30.5, 31, 31.5, 32, 32.5, 33, 33.5, 34, 34.5, 35, 35.5, 36, 36.5, 37, 37.5, 38, 38.5, 39, 39.5, 40, 40.5, 41, 41.5, 42, 42.5, 43, 43.5, 44, 44.5, 45, 45.5, 46, 46.5, 47, 47.5, 48, 48.5, 49, 49.5, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+        105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200,
+    ]
+    range.max = max_times.length - 1
+
+    range.addEventListener('input', function () {
+        const val = range.value
+        const min = range.min ? range.min : 0
+        const max = range.max ? range.max : 100
+        const fraction = Number((val - min) / (max - min))
+        bubble.innerText = String(max_times[val].toFixed(1)).padStart(2, ' ')
+
+        const range_width = range.getBoundingClientRect().width - 36
+        const range_left = range.getBoundingClientRect().left
+        let left = range_left + fraction * range_width
+        if (val < 10) {
+            left += 6
+        } else if (val < 100) {
+            left += 3
+        }
+        bubble.style.left = `${left}px`
+        document.getElementById('sn-dists-age').innerText = bubble.innerText
+        if (max_times[val] >= 50) {
+            bubble.style.backgroundColor = 'var(--primary-dark)'
+        } else {
+            bubble.style.backgroundColor = 'var(--primary)'
+        }
+    })
+    range.addEventListener('mouseover', function () {
+        bubble.style.opacity = 1
+    })
+    range.addEventListener('mouseout', function () {
+        bubble.style.opacity = 0
+    })
+    range.dispatchEvent(new Event('input'))
+
+    const play = document.getElementById('sn-dists-play')
+    const pause = document.getElementById('sn-dists-pause')
+
+    play.addEventListener('click', function () {
+        intervalID = setInterval(function () {
+            let new_value = parseInt(range.value) + direction
+            if (new_value < 0 || new_value > max_times.length - 1) {
+                direction *= -1
+                new_value += direction
+            }
+            range.value = new_value
+            range.dispatchEvent(new Event('input'))
+        }, interval_time)
+        this.disabled = true
+        pause.disabled = false
+    })
+
+    pause.addEventListener('click', function () {
+        clearInterval(intervalID)
+        play.disabled = false
+        this.disabled = true
+    })
+
+    document.getElementById('sn-dists-slower').addEventListener('click', function () {
+        interval_time = Math.round(Math.min(1000, interval_time * 2))
+        pause.click()
+        play.click()
+    })
+
+    document.getElementById('sn-dists-faster').addEventListener('click', function () {
+        interval_time = Math.round(Math.max(15, interval_time / 2))
+        pause.click()
+        play.click()
+    })
+
+    models.forEach((model) => {
+        let el = document.createElement('button')
+        el.classList.add('btn', 'btn-primary', 'sn-dists-button')
+        el.setAttribute('id', 'sn-dists-' + model)
+        el.addEventListener('click', function () {
+            build_sn_dists_histogram(model)
+            document.querySelectorAll('.sn-dists-button').forEach((e) => {
+                e.classList.remove('active')
+            })
+            this.classList.add('active')
+        })
+        el.innerText = model
+        if (model === 'fiducial') {
+            el.classList.add('active')
+        }
+        document.getElementById('sn-dists-switcher').appendChild(el)
     })
 
     document.getElementById('home').addEventListener('click', function () {
@@ -43,48 +137,32 @@ function deepClone(obj) {
     return clone
 }
 
-function make_plot() {
+function build_sn_dists_histogram(model) {
     fetch('../../data/feedback/plotly-feedback-sn-dists-by-time.h5')
         .then((response) => response.arrayBuffer())
         .then(function (buffer) {
             var f = new hdf5.File(buffer, 'plotly-feedback-sn-dists-by-time.h5')
-            histograms(f)
+            histograms(f, model)
         })
 }
 
-function histograms(f) {
+function histograms(f, model) {
     const dark_mode = document.getElementById('dark-mode-checkbox').checked
     let bg = dark_mode ? '#333' : 'white'
     let anti_bg = dark_mode ? 'white' : '#333'
     let grey = dark_mode ? '#595656' : '#eee'
 
     // logs the keys in the HDF5 file
-    console.log(f.get('fiducial').keys)
+    console.log(f.keys)
 
-    let bin_centres = f.get('fiducial/bin_centres').value
-    let bin_widths = f.get('fiducial/bin_widths').value
-    let max_times = f.get('fiducial/max_times').value
-    let hists = reshapeTo3D(f.get('fiducial/histograms').value, 4, max_times.length, bin_centres.length)
+    let bin_centres = f.get('bin_centres').value
+    let bin_widths = f.get('bin_widths').value
+    let max_times = f.get('max_times').value
+    let hists = reshapeTo3D(f.get(model).value, 4, max_times.length, bin_centres.length)
 
     const colours = ['#b778d5', '#4eb89d', '#2a788e', '#87ceeb']
     const labels = ['Merger Product', 'Secondary', 'Primary', 'Effectively Single']
     const n_types = hists.length
-    const n_times = hists[0].length
-    const n_bins = hists[0][0].length
-
-    console.log(bin_centres)
-
-    // let traces = []
-    // for (let i = 0; i < labels.length; i++) {
-    //     traces.push({
-    //         x: bin_centres,
-    //         y: hists[i][122],
-    //         name: labels[i],
-    //         marker: { color: colours[i] },
-    //         type: 'bar',
-    //         hovertemplate: labels[i] + `<br>Distance (pc): %{x}<br> N_SN: %{y}<extra></extra>`,
-    //     })
-    // }// Initial traces for the first time (max_times[0]), stacked by type
     const data = []
     for (let t = 0; t < n_types; t++) {
         data.push({
@@ -116,20 +194,6 @@ function histograms(f) {
             data: frame_data,
         }
     })
-
-    // Define the slider
-    const sliderSteps = max_times.map((time, t_idx) => ({
-        method: 'animate',
-        label: `t ≤ ${time}`,
-        args: [
-            [`t${t_idx}`],
-            {
-                mode: 'immediate',
-                transition: { duration: 0 },
-                frame: { duration: 0, redraw: true },
-            },
-        ],
-    }))
 
     const fs = 20
 
@@ -184,88 +248,21 @@ function histograms(f) {
         displaylogo: false,
     }
 
+    document.getElementById('histograms').innerHTML = ''
+
     Plotly.newPlot('histograms', data, layout, config).then(() => {
         Plotly.addFrames('histograms', frames)
 
-        const range = document.getElementById('profile-slider')
-        const bubble = document.getElementById('profile-slider-bubble')
-        range.max = max_times.length - 1
-
-        range.addEventListener('input', function () {
-            const val = range.value
-            const min = range.min ? range.min : 0
-            const max = range.max ? range.max : 100
-            const fraction = Number((val - min) / (max - min))
-            bubble.innerText = String(max_times[val].toFixed(1)).padStart(2, ' ')
-
-            const range_width = range.getBoundingClientRect().width - 36
-            const range_left = range.getBoundingClientRect().left
-            let left = range_left + fraction * range_width
-            if (val < 10) {
-                left += 6
-            } else if (val < 100) {
-                left += 3
-            }
-            bubble.style.left = `${left}px`
-            document.getElementById('profile-age').innerText = bubble.innerText
-            if (max_times[val] >= 50) {
-                bubble.style.backgroundColor = 'var(--primary-dark)'
-            } else {
-                bubble.style.backgroundColor = 'var(--primary)'
-            }
-        })
-        range.addEventListener('mouseover', function () {
-            bubble.style.opacity = 1
-        })
-        range.addEventListener('mouseout', function () {
-            bubble.style.opacity = 0
-        })
-        range.dispatchEvent(new Event('input'))
-
-        const play = document.getElementById('profile-play')
-        const pause = document.getElementById('profile-pause')
-
-        play.addEventListener('click', function () {
-            intervalID = setInterval(function () {
-                let new_value = parseInt(range.value) + direction
-                if (new_value < 0 || new_value > max_times.length - 1) {
-                    direction *= -1
-                    new_value += direction
-                }
-                range.value = new_value
-                range.dispatchEvent(new Event('input'))
-            }, interval_time)
-            this.disabled = true
-            pause.disabled = false
-        })
-
-        pause.addEventListener('click', function () {
-            clearInterval(intervalID)
-            play.disabled = false
-            this.disabled = true
-        })
-
-        document.getElementById('profile-slower').addEventListener('click', function () {
-            interval_time = Math.round(Math.min(1000, interval_time * 2))
-            pause.click()
-            play.click()
-        })
-
-        document.getElementById('profile-faster').addEventListener('click', function () {
-            interval_time = Math.round(Math.max(15, interval_time / 2))
-            pause.click()
-            play.click()
-        })
-
-        // actual plot animation
-
-        document.getElementById('profile-slider').addEventListener('input', function () {
+        document.getElementById('sn-dists-slider').removeEventListener('input', animate_hist)
+        function animate_hist() {
             Plotly.animate('histograms', frames[this.value], {
                 mode: 'immediate',
                 transition: { duration: 0 },
                 frame: { duration: 0, redraw: true },
             })
-        })
+        }
+
+        document.getElementById('sn-dists-slider').addEventListener('input', animate_hist)
 
         document.getElementById('stack-mode').addEventListener('click', function () {
             Plotly.relayout('histograms', {
@@ -278,7 +275,9 @@ function histograms(f) {
             this.classList.add('active')
             document.getElementById('separate-mode').classList.remove('active')
         })
-        document.getElementById('separate-mode').addEventListener('click', function () {
+
+        const sep_mode = document.getElementById('separate-mode')
+        sep_mode.addEventListener('click', function () {
             Plotly.relayout('histograms', {
                 barmode: 'group',
                 yaxis: {
@@ -289,10 +288,15 @@ function histograms(f) {
             this.classList.add('active')
             document.getElementById('stack-mode').classList.remove('active')
         })
-        document.getElementById('profile-play').click()
+
+        document.getElementById('sn-dists-play').click()
         setTimeout(function () {
-            document.getElementById('profile-pause').click()
+            document.getElementById('sn-dists-pause').click()
         }, 100)
+
+        if (sep_mode.classList.contains('active')) {
+            sep_mode.click()
+        }
     })
 }
 
